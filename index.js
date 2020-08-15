d4c5f29f613fee263be87e7501c59a8e7953e668
wisdomosara <wisdomosara@gmail.com>
2020-08-15 15:20:29 +0100

    added

---
+++ index.js
@@ -0,0 +1,9 @@
+
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.font = "72px Source Sans Pro";
+// Create gradient
+
+// Fill with gradient
+ctx.strokeStyle = 'white';
+ctx.strokeText("DIGITAL PRODUCT", 10, 84);
