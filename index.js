2d75a367775a2e280a161888bcb00d219e05b74d
wisdomosara <wisdomosara@gmail.com>
2020-08-16 19:00:20 +0100

    added

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+let clicked = false
+let nav = document.querySelector(".mobile-nav")
 function myFunction(x) {
+    clicked = !clicked
     x.classList.toggle("close");
-  }
+    if(clicked) {
+        
+        nav.style.right = "0"
+    }else {
+        nav.style.right = "100%"
+    }
+    
+}
+    
+  
