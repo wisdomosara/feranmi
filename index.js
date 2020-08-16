c24381064ca61e06a7ae24c38942d05c81b0d4c0
wisdomosara <wisdomosara@gmail.com>
2020-08-16 18:03:41 +0100

    added

--- index.js
+++
@@ -1,9 +0,0 @@
-
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-ctx.font = 0.06 * window.innerWidth + "px Source Sans Pro";
-// Create gradient
-
-// Fill with gradient
-ctx.strokeStyle = 'white';
-ctx.strokeText("DIGITAL PRODUCT", 10, 84);
