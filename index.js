bc1e3a01dd1690c9a1f894df761b3cea1835628c
wisdomosara <wisdomosara@gmail.com>
2020-08-17 02:22:46 +0100

    added

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 let clicked = false
+let but1 = document.querySelector(".mobile")
 let nav = document.querySelector(".mobile-nav")
+let navLinks = document.querySelectorAll(".mobile-nav ul li a");
+navLinks.forEach(link => link.addEventListener("click", function() {
+    clicked = !clicked;
+    but1.classList.toggle("close");
+    if(clicked) {
+        
+        nav.style.right = "0"
+        document.querySelector(".logo span").style.color = "white"
+    }else {
+        nav.style.right = "100%";
+        document.querySelector(".logo span").style.color = "#5132C0"
+    }
+}))
 function myFunction(x) {
     clicked = !clicked
     x.classList.toggle("close");
     if(clicked) {
         
         nav.style.right = "0"
+        document.querySelector(".logo span").style.color = "white"
     }else {
-        nav.style.right = "100%"
+        nav.style.right = "100%";
+        document.querySelector(".logo span").style.color = "#5132C0"
     }
-    
+
     
 }
     
